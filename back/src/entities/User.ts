341c0f1ead65f81254de232d22910cdbca209415
lucasflins <lucas_flins@hotmail.com>
2021-06-26 15:05:45 -0300

    Creating-User

---
+++ back/src/entities/User.ts
@@ -0,0 +1,32 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+	@PrimaryColumn()
+	id: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	passwordHash: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	user_name: string;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+
+	constructor() {
+		if (!this.id) {
+			this.id = uuid();
+		}
+	}
+}
+
+export { User };
