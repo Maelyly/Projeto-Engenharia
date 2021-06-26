24ffe81322a6f4614542989c458f2248dcc41072
lylyPC <maelyalways@gmail.com>
2021-06-26 20:50:47 -0300

    add frontend made by Fernando

---
+++ frontend/src/components/cadastro/cadastro.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './cadastro.css';
+import {Link } from "react-router-dom";
+export default function Cadastro() {
+  return(
+    <div>
+
+        <h2 className= "subTitulo-Cadastro">
+            Crie sua conta
+        </h2>
+
+        <form className = "box">
+        <label>
+            <p className = "nome-cadastro">Digite seu nome</p>
+            <input type="text"  className= "posicaoNomeCadastro" />
+        </label>
+        <label>
+            <p className = "email-cadastro">Digite seu email</p>
+            <input type="email" className= "posicaoEmailCadastro" />
+        </label>
+        <label>
+            <p className = "senha-cadastro">Crie sua senha</p>
+            <input type="password" className= "posicaoSenhaCadastro" />
+        </label>
+        <label>
+            <p className = "senha-cadastro2">Confirme sua senha</p>
+            <input type="password" className= "posicaoSenhaCadastro2" />
+        </label>
+        <div>
+            <Link to= "/login">
+            <button  type="submit" className = "posicaoBotaoCancelar" >Cancelar</button>
+            </Link>
+            <Link to= "/home">
+            <button type="submit" className = "posicaoBotaoCriar" >Criar</button>
+            </Link>
+        </div>
+        </form>
+
+        <div className="login-asth">
+            <h1 className= "titulo">
+                Compras
+                Facil
+            </h1>
+            <p className= "slogan">    
+            Seja bem-vindo!
+            Ao meu, ao seu, ao nosso gerenciador de compras :)
+            </p>
+        </div>
+
+    </div>
+  );
+}
