e98e388d4964532ccd6573b3b7b921c5cdf2ac17
lucasflins <lucas_flins@hotmail.com>
2021-06-30 21:53:47 -0300

    Addind Auth user - not tested

---
+++ back/src/body/AuthBody.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from '../entities/User'
+import { UsersRepository } from '../repositories/UsersRepository';
+import { compareSync } from 'bcrypt';
+//import { config as dotenvConfig } from "dotenv";
+import *  as jwt from 'jsonwebtoken';
+import { json } from 'express';
+//dotenvConfig()
+
+class AuthBody {
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository)
+    }
+
+    async authenticate(user_name: string, password: string) {
+        const user = await this.usersRepository.findOne({ user_name });
+
+		if (!user) return false;
+
+		const hash = user.passwordHash;
+
+		const isCorrectPassword = compareSync(password, hash);
+
+		if (isCorrectPassword) {
+			console.log(process.env.ACCESS_TOKEN_SECRET);
+			const accessToken = jwt.sign({ "username": user.user_name }, process.env.ACCESS_TOKEN_SECRET);
+			return accessToken;
+		}
+
+		return isCorrectPassword;
+    }
+
+} 
+
+export { AuthBody }
