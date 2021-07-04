8fed5ecb4ae15ddc3153ec36bd5825174ec0552d
lylyPC <maelyalways@gmail.com>
2021-07-04 20:29:03 -0300

    routes atualize

--- back/src/routes.ts
+++ back/src/routes.ts
@@ -4,6 +4,8 @@ import { AuthController } from './controllers/AuthController';
 import { ItemsController } from './controllers/itemsController';
 import { ProductsController } from './controllers/productsController';
 import './database';
+import { ShoppingListController } from './controllers/ShoppingListController';
+import { ShoppingItemController } from './controllers/ShoppingItemController';
 
 const routes = Router();
 
@@ -11,6 +13,8 @@ const usersController = new UsersController();
 const authController = new AuthController();
 const itemsController = new ItemsController();
 const productsController = new ProductsController();
+const shoppinglistController = new ShoppingListController();
+const shoppingitemController = new ShoppingItemController();
 
 routes.get('/', (request, response) => {
 	return response.json({
@@ -25,5 +29,8 @@ routes.post('/items', itemsController.create);
 routes.get('/items', itemsController.list);
 routes.post('/products', productsController.create);
 routes.get('/products', productsController.list);
-
+routes.post('/shoppinglist', shoppinglistController.create);
+routes.get('/shoppinglist', shoppinglistController.list);
+routes.post('/shoppingitem', shoppingitemController.create);
+routes.get('/shoppingitem', shoppingitemController.list);
 export { routes };
