1501d200af5990908f12ac8143796c1134209ce2
FernandoWeeb-byte <fernandobr7186@gmail.com>
2021-07-03 21:41:25 -0300

    login-logout

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logo from './../../images/logo/logo.png';
 import './home.css'
 import StickyHeadTable from './compra';
+import AuthContext from '../../store/authContext';
 
 export default function Home() {
+  const austCtx = useContext(AuthContext);
+  const isLoggedIn = austCtx.isLoggedIn;
+
+  function logoutHandler(){
+    austCtx.logout();
+  };
+  
   return(
     <div>
     <h2 className="titulo1">Compras Facil</h2>
+    {isLoggedIn && (<label className="teste">
+    <p>vc esta logado </p>
+    </label>)}
+    <button className= "botaoTeste" onClick = {logoutHandler}>
+      logout
+    </button>
+    
     <img src={logo} alt="logo" className="LogoHome" />
 
       <StickyHeadTable/>
--- frontend/src/components/login/loginForm.js
+++ frontend/src/components/login/loginForm.js
@@ -1,6 +1,6 @@
 import React,{useEffect,useState,useContext} from 'react';
 import './login.css';
-import {Link } from "react-router-dom";
+import {Link, useHistory } from "react-router-dom";
 import axios from 'axios';
 import api from '../../services/api';
 import AuthContext from '../../store/authContext';
@@ -9,6 +9,8 @@ export default function LoginForm(){
     const [email,setEmail] = useState('');
     const [senha,setSenha] = useState('');
     const authCtx = useContext(AuthContext);
+    const isLoggedIn = authCtx.isLoggedIn;
+    const history = useHistory();
 
 
     const changeHandlerEmail = (event) => {
@@ -38,8 +40,9 @@ export default function LoginForm(){
         }
         console.log(userData);
         try{
-            const response = await api.post('auth/login', userData)
-            authCtx.login(response)
+           const response = await api.post('auth/login', userData)
+            authCtx.login(response.data)
+            history.replace('/home')
             console.log('Response:', response.userData);
             console.log(response)
         }catch(error){
@@ -60,10 +63,12 @@ export default function LoginForm(){
             <input type="password" className= "posicaoSenha" onChange= {changeHandlerSenha}/>
         </label>
         <div>
+            
             <button  type="submit" className = "posicaoBotaoEntrar" >Entrar</button>
             <Link to= "/cadastro">
             <button className = "posicaoBotaoCadastrar" >Cadastrar</button>
             </Link>
+           
         </div>
         </form>
     )
